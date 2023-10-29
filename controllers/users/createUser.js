af756acf007a863bf1c095ae0c49cb41976e9145
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2023-10-29 11:54:55 +0530

    added email field to db

--- controllers/users/createUser.js
+++ controllers/users/createUser.js
@@ -2,14 +2,18 @@ const User = require("../../models/users.model")
 const bcrypt = require("bcrypt")
 
 const createUser = async (req, res) => {
-  const { username, password } = req.body
+  const { username, email, password } = req.body
   try {
     bcrypt.hash(password, 3, async (err, hashedpw) => {
       if (err) {
         res.status(400).send({ error: err })
       }
       if (hashedpw) {
-        const newUser = await User.create({ username, password: hashedpw })
+        const newUser = await User.create({
+          username,
+          email,
+          password: hashedpw,
+        })
         res
           .status(200)
           .send({ message: "User created successfully.", user: newUser })
